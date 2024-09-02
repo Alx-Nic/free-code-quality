58a75735a39a7ddd8c0ce2ba411616c0129bb9a3
Alexandru Nicula <alexandru.nicula@apuliasoft.com>
2024-09-02 14:47:34 +0000

    fix

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const { ObjectID } = require("mongodb");
 const LocalStrategy = require("passport-local");
 
 const app = express();
+app.set("view engine", "pug");
+app.set("views", "./views/pug");
+
 const showLogin = true;
 const showRegistration = true;
 
@@ -16,8 +19,6 @@ fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "pug");
-app.set("views", "./views/pug");
 
 app.use(
   session({
@@ -28,8 +29,8 @@ app.use(
   })
 );
 
-passport.initialize();
-passport.session();
+app.use(passport.initialize());
+app.use(passport.session());
 
 myDB(async (client) => {
   const myDataBase = await client.db("database").collection("users");
