635ebc0b306c164d0424d41de729da9b3dd675c0
Alexandru Nicula <alexandru.nicula@apuliasoft.com>
2024-09-02 14:48:53 +0000

    fix

--- server.js
+++ server.js
@@ -2,23 +2,16 @@
 require("dotenv").config();
 const express = require("express");
 const myDB = require("./connection");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const session = require("express-session");
 const passport = require("passport");
-const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const { ObjectID } = require("mongodb");
 const LocalStrategy = require("passport-local");
 
 const app = express();
+
 app.set("view engine", "pug");
 app.set("views", "./views/pug");
-
-const showLogin = true;
-const showRegistration = true;
-
-fccTesting(app); //For FCC testing purposes
-app.use("/public", express.static(process.cwd() + "/public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
@@ -32,14 +25,19 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+fccTesting(app); // For fCC testing purposes
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 myDB(async (client) => {
   const myDataBase = await client.db("database").collection("users");
 
-  app.route("/").get(async (req, res) => {
+  app.route("/").get((req, res) => {
     res.render("index", {
       title: "Connected to Database",
       message: "Please log in",
-      showLogin,
+      showLogin: true,
     });
   });
 
@@ -48,12 +46,11 @@ myDB(async (client) => {
     .post(
       passport.authenticate("local", { failureRedirect: "/" }),
       (req, res) => {
-        console.log(`User ${req.user.username} has logged in.`);
         res.redirect("/profile");
       }
     );
 
-  app.route("/profile").get((req, res) => {
+  app.route("/profile").get(ensureAuthenticated, (req, res) => {
     res.render("profile");
   });
 
@@ -61,33 +58,43 @@ myDB(async (client) => {
     new LocalStrategy((username, password, done) => {
       myDataBase.findOne({ username: username }, (err, user) => {
         console.log(`User ${username} attempted to log in.`);
-        if (err) return done(err);
-        if (!user) return done(null, false);
-        if (password !== user.password) return done(null, false);
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false);
+        }
+        if (password !== user.password) {
+          return done(null, false);
+        }
         return done(null, user);
       });
     })
   );
 
-  passport.serializeUser((user, doneCB) => {
-    doneCB(null, user._id);
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
   });
 
-  passport.deserializeUser((id, doneCB) => {
+  passport.deserializeUser((id, done) => {
     myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      doneCB(null, doc);
+      done(null, doc);
     });
   });
 }).catch((e) => {
   app.route("/").get((req, res) => {
-    res.render("pug", {
-      title: "e",
-      message: "Unable to connect to database",
-    });
+    res.render("index", { title: e, message: "Unable to connect to database" });
   });
 });
 
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Listening on port " + PORT);
+  console.log(`Listening on port ${PORT}`);
 });
