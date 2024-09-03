2afa9e91e026441d769c5c102c27b6fb12d84b08
Alexandru Nicula <alexandru.nicula@apuliasoft.com>
2024-09-03 08:29:21 +0000

    github strategy

--- auth.js
+++ auth.js
@@ -1,5 +1,6 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+const GitHubStrategy = require("passport-github").Strategy;
 const bcrypt = require("bcrypt");
 const { ObjectID } = require("mongodb");
 
@@ -29,5 +30,20 @@ module.exports = function (app, myDataBase) {
         return done(null, user);
       });
     })
+  );
+
+  passport.use(
+    new GitHubStrategy(
+      {
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL:
+          "https://4a446bdd-b702-4b3f-9880-64392ff3efe6-00-20lxmhy7suhf2.worf.replit.dev/auth/github/callback",
+      },
+      function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+        //Database logic here with callback containing your user object
+      }
+    )
   );
 };
