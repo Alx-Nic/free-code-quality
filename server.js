bd643cf9d44903519323d28e2b34c2984a966ece
Alexandru Nicula <alexandru.nicula@apuliasoft.com>
2024-09-03 09:13:59 +0000

    emit

--- server.js
+++ server.js
@@ -39,8 +39,13 @@ myDB(async (client) => {
   routes(app, myDataBase);
   auth(app, myDataBase);
 
+  let currentUsers = 0;
   io.on("connection", (socket) => {
     console.log("A user has connected");
+
+    ++currentUsers;
+
+    io.emit("usercount", currentUsers);
   });
 
   app.use((req, res, next) => {
