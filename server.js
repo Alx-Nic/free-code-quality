07d75bd820bee49a37696f7c221650db1592fcac
Alexandru Nicula <alexandru.nicula@apuliasoft.com>
2024-09-03 09:39:05 +0000

    chat message

--- server.js
+++ server.js
@@ -64,6 +64,13 @@ myDB(async (client) => {
       connected: true,
     });
 
+    socket.on("chat message", (message) => {
+      io.emit("chat message", {
+        username: socket.request.user.username,
+        message,
+      });
+    });
+
     socket.on("disconnect", () => {
       console.log("A user has disconnected");
       --currentUsers;
